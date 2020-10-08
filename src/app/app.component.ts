784292e3241d167a8e11958ee64ea22baa1b2de9
borys25ol <oleynik.boris@gmail.com>
2020-10-09 00:04:09 +0300

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Store } from '@ngrx/store'
+
+import { AppState } from './types/app-state.interface'
+import { Observable } from 'rxjs'
+import { ShoppingItem } from './types/shopping-item.interface'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-ngrx'
+export class AppComponent implements OnInit {
+  shoppingItems$: Observable<Array<ShoppingItem>>
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.initializeValues()
+  }
+
+  initializeValues(): void {
+    this.shoppingItems$ = this.store.select((store) => store.shopping)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { StoreModule } from '@ngrx/store'
 
 import { AppComponent } from './app.component'
+import { shoppingReducer } from './store/reducers/shopping.reducer'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({ shopping: shoppingReducer }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
