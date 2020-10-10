e56c870c1cf3c9e10cebf3f2d52818f7a9a387a0
borys25ol <oleynik.boris@gmail.com>
2020-10-10 18:03:14 +0300

    Change actions and reducers to modern syntax

--- src/app/store/actions/shopping.actions.ts
+++ src/app/store/actions/shopping.actions.ts
@@ -1,16 +1,12 @@
-import { Action } from '@ngrx/store'
+import { Action, createAction, props } from '@ngrx/store'
 
-import { ShoppingItem } from '../../types/shopping-item.interface'
+import { ShoppingItemInterface } from '../../types/shopping-item.interface'
 
 export enum ShoppingActionTypes {
-  ADD_ITEM = '[SHOPPING] Add Item'
+  ADD_ITEM = '[SHOPPING] Add Item',
 }
 
-export class AddItemAction implements Action {
-  readonly type = ShoppingActionTypes.ADD_ITEM
-
-  constructor(public payload: ShoppingItem) {
-  }
-}
-
-export type ShoppingAction = AddItemAction
+export const addItemAction = createAction(
+  ShoppingActionTypes.ADD_ITEM,
+  props<{ shoppingItem: ShoppingItemInterface }>()
+)
--- src/app/store/reducers/shopping.reducer.ts
+++ src/app/store/reducers/shopping.reducer.ts
@@ -1,7 +1,13 @@
-import { ShoppingItem } from '../../types/shopping-item.interface'
-import { ShoppingAction, ShoppingActionTypes } from '../actions/shopping.actions'
+import { ShoppingItemInterface } from '../../types/shopping-item.interface'
+import { Action, createReducer, on } from '@ngrx/store'
 
-const initialState: Array<ShoppingItem> = [
+import {
+  addItemAction,
+  ShoppingActionTypes,
+} from '../actions/shopping.actions'
+import { AppStateInterface } from '../../types/app-state.interface'
+
+const initialState: Array<ShoppingItemInterface> = [
   {
     id: '624c72c0-c699-4616-b04b-9bed14e554bc',
     name: 'Test Product Name 1',
@@ -12,15 +18,16 @@ const initialState: Array<ShoppingItem> = [
   },
 ]
 
-export function shoppingReducer(
-  state: Array<ShoppingItem> = initialState,
-  action: ShoppingAction
-) {
-  switch (action.type) {
-    case ShoppingActionTypes.ADD_ITEM:
-      return [...state, action.payload]
+const shoppingReducer = createReducer(
+  initialState,
+  on(
+    addItemAction,
+    (state, action): Array<ShoppingItemInterface> => {
+      return [...state, action.shoppingItem]
+    }
+  )
+)
 
-    default:
-      return state
-  }
+export function reducer(state: Array<ShoppingItemInterface>, action: Action) {
+  return shoppingReducer(state, action)
 }
