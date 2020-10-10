54443bca2c06cdf2935b1c20bb550ee5188ea8f3
borys25ol <oleynik.boris@gmail.com>
2020-10-10 18:39:57 +0300

    Update component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core'
-import { Store } from '@ngrx/store'
+import { select, Store } from '@ngrx/store'
+import { v4 as uuid } from 'uuid'
 
-import { AppState } from './types/app-state.interface'
+import { AppStateInterface } from './types/app-state.interface'
 import { Observable } from 'rxjs'
-import { ShoppingItem } from './types/shopping-item.interface'
+import { ShoppingItemInterface } from './types/shopping-item.interface'
+import {
+  addItemAction,
+  deleteItemAction,
+} from './store/actions/shopping.actions'
+import { shoppingSelector } from './store/selectors/shopping.selector'
 
 @Component({
   selector: 'app-root',
@@ -11,15 +17,28 @@ import { ShoppingItem } from './types/shopping-item.interface'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  shoppingItems$: Observable<Array<ShoppingItem>>
+  shoppingItems$: Observable<Array<ShoppingItemInterface>>
+  newShoppingItem: ShoppingItemInterface = { id: '', name: '' }
 
-  constructor(private store: Store<AppState>) {}
+  constructor(private store: Store<AppStateInterface>) {}
 
   ngOnInit(): void {
     this.initializeValues()
   }
 
   initializeValues(): void {
-    this.shoppingItems$ = this.store.select((store) => store.shopping)
+    this.shoppingItems$ = this.store.pipe(select(shoppingSelector))
+  }
+
+  addItem(): void {
+    if (this.newShoppingItem.name.trim()) {
+      this.newShoppingItem.id = uuid()
+      this.store.dispatch(addItemAction({ shoppingItem: this.newShoppingItem }))
+      this.newShoppingItem = { id: '', name: '' }
+    }
+  }
+
+  deleteItem(id: string): void {
+    this.store.dispatch(deleteItemAction({ id }))
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 
 import { AppComponent } from './app.component'
-import { shoppingReducer } from './store/reducers/shopping.reducer'
+import { reducer } from './store/reducers/shopping.reducer'
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
-    StoreModule.forRoot({ shopping: shoppingReducer }),
+    StoreModule.forRoot({ shopping: reducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
